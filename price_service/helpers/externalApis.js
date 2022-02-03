85eaf70223b546424916ca4d4f82ba0f494eafaa
Manos Pappas <mano.pappa@gmail.com>
2022-02-03 19:38:37 +0000

    MW-04 Fullfil concurrent requests with on external + Add comments

--- price_service/helpers/externalApis.js
+++ price_service/helpers/externalApis.js
@@ -1,13 +1,38 @@
 const axios = require('axios');
+const externalRequestsMap = new Map();
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 /**
  * Prices 3rd party API
- * @return CarPrice
+ * @return { NumberPlate: CarPrice }
  */
 async function getExternalPrice(numberPlate) {
   try {
-    const response = await axios.get(`http://localhost:3000/${numberPlate}`);
-    return response.data;
+    let inFlightRequest = externalRequestsMap.get(numberPlate);
+    // Wait for a minute while a request has already been sent with status pending
+    while (inFlightRequest === 'pending') {
+      sleep(6000);
+      // After waiting for a minute check again for the status of the request
+      inFlightRequest = externalRequestsMap.get(numberPlate);
+      break;
+    }
+
+    if (inFlightRequest && inFlightRequest != 'pending') {
+      return { plateNumber: numberPlate, price: inFlightRequest };
+    }
+
+    // First time request
+    if (!inFlightRequest) {
+      // Initial record of in-flight request
+      externalRequestsMap.set(numberPlate, 'pending');
+      const response = await axios.get(`http://localhost:3000/${numberPlate}`);
+      // Initial record of in-flight request's value with price
+      externalRequestsMap.set(numberPlate, response.data.price);
+      return response.data;
+    }
   } catch (error) {
     console.error('Error retrieving price for the specified numer plate', error);
   }
--- price_service/prices/pricesController.js
+++ price_service/prices/pricesController.js
@@ -2,25 +2,33 @@
 
 const { getExternalPrice } = require('../helpers/externalApis');
 
+/**
+ * GetPrice helper function
+ * @return { NumberPlate: CarPrice }
+ */
 async function getPrice(numberPlate, skipCacheForRead = true) {
+  // Check if record is in cache and return to avoid external request
   if (skipCacheForRead === false) {
-    console.log('I am here');
     const cachedPrice = await cache.get(numberPlate);
     console.log(`Cached price for number plate ${numberPlate} is ${cachedPrice}`);
     if (cachedPrice) return { plateNumber: numberPlate, price: cachedPrice };
   }
 
+  // In case price hasn't been cached call external request helper function
   const externalRequest = await getExternalPrice(numberPlate);
+  // and set the new record in cache for future refernce
   let cacheNewPrice = await cache.set(externalRequest.plateNumber, externalRequest.price);
-  console.log('Cache', cacheNewPrice);
   return externalRequest;
 }
 
+/**
+ * GET Request handler
+ * @return response status + { NumberPlate: CarPrice }
+ */
 async function httpGetCarPrice(req, res) {
   const numberPlate = req.params.id;
   let skipCache = req.params.cache;
 
-  console.log("Prices Controller - I've been called");
   return res.status(200).json(await getPrice(numberPlate, skipCache));
 }
 
